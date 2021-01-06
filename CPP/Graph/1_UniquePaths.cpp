eb041227acc209aba485aafe070eacd1a26f8645
Lalit Shanker Choudhary <51866797+lalithub@users.noreply.github.com>
2021-01-06 10:07:12 +0900

    Create 1_UniquePaths.cpp
    Leetcode: 62. Unique Paths - Meduim

---
+++ CPP/Graph/1_UniquePaths.cpp
@@ -0,0 +1,55 @@
+/*
+Leetcode: 62. Unique Paths - Meduim
+https://leetcode.com/problems/unique-paths/
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+
+Example 1: Input: m = 3, n = 7 Output: 28
+Example 2: Input: m = 3, n = 2 Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+Example 3: Input: m = 7, n = 3 Output: 28
+Example 4: Input: m = 3, n = 3 Output: 6
+
+Constraints:
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int>dp(m, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+};
+
+void testSolution(int m, int n, int paths)
+{
+    Solution s;
+    int res = s.uniquePaths(m, n);
+    cout << "m: " << m << " n: " << n << " result: " << res << ( (paths == res) ? " - PASS" : " - FAIL") << endl;
+    cout << "----------------------------" << endl;
+}
+
+int main() {
+    testSolution(3, 7, 28);
+    testSolution(3, 2, 3);
+    testSolution(3, 3, 6);
+    testSolution(7, 3, 28);
+	return 0;
+}
